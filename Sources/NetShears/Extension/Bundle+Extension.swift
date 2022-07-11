d25ba3bcd2ae9a7e86a049e3159830f76da4f418
Mehdi Mirzaie <mehdiimrz@gmail.com>
2022-07-12 01:46:30 +0430

    Fix build warnings (#21)
    Co-authored-by: Mehdi Mirzaei <mehdi.mirzaie@divar.ir>

--- Sources/NetShears/Extension/Bundle+Extension.swift
+++ Sources/NetShears/Extension/Bundle+Extension.swift
@@ -12,14 +12,14 @@ extension Bundle {
         #if SWIFT_PACKAGE
         let resourceBundle = Bundle.module
         return resourceBundle
-        #endif
+        #else
         let podBundle = Bundle(for: NetShears.classForCoder())
         if let bundleURL = podBundle.url(forResource: "NetShears", withExtension: "bundle"){
             if let bundle = Bundle(url: bundleURL) {
                 return bundle
             }
         }
-        
         return Bundle(for: NetShears.classForCoder())
+        #endif
     }
 }
